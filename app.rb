68bda09b13ed1b3f6ba8f4ccbbcbfd2aea3a3a5c
destinybbrianna <destinybbrianna@gmail.com>
2018-08-22 20:12:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,18 +1,46 @@
 require_relative 'config/environment'
 
-class App < Sinatra::Base
+ class App < Sinatra::Base
 
-  # This is a sample static route.
-  get '/hello' do
+   # This is a sample static route.
+   get '/hello' do
     "Hello World!"
-  end
+   end
 
-  # This is a sample dynamic route.
-  get "/hello/:name" do
-    @user_name = params[:name]
-    "Hello #{@user_name}!"
-  end
+   # This is a sample dynamic route.
+   get "/hello/:name" do
+    puts params 
+    
+     @user_name = params[:name]
+     "Hello #{@user_name}!"
+   end
+   get '/goodbye' do 
+    'Goodbye'
+  end 
+  
+  get "/goodbye/:name" do 
+    params 
+       @user_name = params[:name]
+       "Goodbye #{@user_name}"
+    erb :goodbye
+     end 
+     
+   end 
 
+
+#Create a new route for /goodbye/:name that returns "goodbyes, (persons name)"
   # Code your final two routes here:
+  
+ 
+  #   get '/goodbye' do 
+  #   'Goodbye'
+  # end 
+  
+  # get "/goodbye/:name" do 
+  #   params 
+  #     @user_name = params[:name]
+  #     "Goodbye #{@user_name}"
+  #   end 
 
-end
+
+
